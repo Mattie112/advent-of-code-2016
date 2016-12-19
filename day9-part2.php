959cabed93fde7b7a87f708b9cda8b7d95217126
Matthijs van Schooten <mattie112@users.noreply.github.com>
2016-12-19 19:24:25 +0000

    day9part2

--- day9-part2.php
+++ day9-part2.php
@@ -1,60 +1,31 @@
 <?php
 
 $handle = fopen("day9.txt", "r");
-$iiii = 0;
 if ($handle) {
     while (($line = fgets($handle)) !== false) {
         $line = trim($line);
 
-        $fetch_compressed_text = false;
-        $compression = "";
-        $decompressed = [];
+        $decompressed = 0;
 
-        while ($line != "") {
-            // From the start of the string search for regular letters
-            preg_match('/^(\w+)/', $line, $parts);
-            if ($parts && !$fetch_compressed_text) {
-                $decompressed[] = $parts[1];
-                $line = substr($line, strlen($parts[0]));
-            }
+        $decompressed = parsePart($line);
 
-            // From the start of the string search for (aaaaa) and capture "aaaaa"
-            preg_match('/^\((\w+)\)/', $line, $parts);
-            if ($parts && !$fetch_compressed_text) {
-                $fetch_compressed_text = true;
-                $compression = $parts[1];
-                $line = substr($line, strlen($parts[0]));
-            }
-
-            if ($fetch_compressed_text) {
-                // Simply fetch some stuff and repeat it
-                preg_match('/(\d+)x(\d+)/', $compression, $parts);
-                $length = $parts[1];
-                $amount = $parts[2];
-
-                // First grab the compressed text
-                $text = substr($line, 0, $length);
-                // Now remove the length from the line
-                $line = substr($line, $parts[1]);
-
-                // Simply prepend line with the decompressed string
-                // By prepenting it to line it may be decompressed again when needed
-                // if not the regex that matched words is triggered saving the output
-                for ($i = 0; $i < $amount; $i++) {
-                    $line = $text . $line;
-                }
-                $compression = null;
-                $fetch_compressed_text = false;
-            }
-            if ($iiii % 100 == 0) {
-                echo strlen($line).PHP_EOL;
-            }
-            $iiii++;
-        }
-        echo "Day 8 answer 2 : " . strlen(join("", $decompressed)) . " " . PHP_EOL;
+        echo "Day 8 answer 2 : " . $decompressed . " " . PHP_EOL;
     }
     fclose($handle);
 } else {
     // Error opening the file.
 }
 
+// Stolen from https://github.com/jvwag/advent-of-code-2016/blob/master/src/Year2016/Day9.php as I was unable to find a nice solution
+function parsePart($str)
+{
+    $total = 0;
+    while (preg_match("/([A-Z]+)?\\(([0-9]+)x([0-9]+)\\)(.*)$/", $str, $match)) {
+        list(, $prepend, $len, $mul, $rest) = $match;
+        $res = parsePart(substr($rest, 0, $len));
+        $total += strlen($prepend) + ($res * $mul);
+        $str = substr($str, strlen("(" . $len . "x" . $mul . ")") + strlen($prepend) + $len);
+    }
+
+    return strlen($str) + $total;
+}
